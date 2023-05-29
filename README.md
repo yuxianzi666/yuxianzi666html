def52db0c092a9c11be18f4bf5fd0cc1c6df68eb
yuxianzi666 <yuxianzi666@outlook.com>
2023-05-29 23:58:04 +0800

    2023.05.30

---
+++ README.md
@@ -0,0 +1,2 @@
+网站主页：https://yuxianzihtml.netlify.app/</br>
+网站备用主页：https://yuxianzi666.github.io/yuxianziHTML</br>
---
+++ js/index.js
@@ -0,0 +1,16 @@
+var tittle = document.getElementById("tittle");
+tittle.onclick = function(){  //标题点击事件
+    var color1=Math.round(Math.random()*15).toString(16);
+    var color2=Math.round(Math.random()*15).toString(16);
+    var color3=Math.round(Math.random()*15).toString(16);
+    var color4=Math.round(Math.random()*15).toString(16);
+    var color5=Math.round(Math.random()*15).toString(16);
+    var color6=Math.round(Math.random()*15).toString(16);
+    var size=Math.round(Math.random()*30+40);
+    tittle.style.fontSize=size+"px";
+    tittle.style.color="#"+color1+color2+color3+color4+color5+color6;
+}
+
+document.getElementById("music1").style.display=""; //控制播放器是否显示
+document.getElementById("email").style.display=""; //控制邮箱是否显示
+document.getElementById("foot").style.display="none"; //控制GitHub项目主页：作者yuxianzi666是否显示
