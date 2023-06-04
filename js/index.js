2fd8e935821531e85907b1acbda77de99981242a
yuxianzi666 <yuxianzi666@outlook.com>
2023-06-04 17:30:41 +0800

    2023.06.04

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function menu_index(){
+    window.location.reload(); //点击首页刷新
+}
+function menu_website(){
+    window.open("./html/404.html",'_blank'); //点击网站打开新窗口
+}
+function menu_status(){
+    window.open("https://stats.uptimerobot.com/Qrv7OhBEBZ",'_blank'); //点击网站运行状态打开新窗口
+}
+function menu_about(){
+    window.open("./html/404.html",'_blank'); //点击网站打开新窗口
+}
