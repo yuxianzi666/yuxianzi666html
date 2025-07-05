5cfcb31f5bf59134c0c3cd1dd9059620eb1f04c4
yuxianzi666 <52189074+yuxianzi666@users.noreply.github.com>
2025-07-05 20:03:34 +0800

    2025/7/5

--- js/a1_MT3000.js
+++ js/a1_MT3000.js
@@ -1,3 +1,3 @@
 function a1_MT3000_passwall(){
-    window.open("./PassWall_25.5.8_aarch64_a53_all_sdk_22.03.7.run"); //点击网站打开新窗口
+    window.open("./PassWall_25.7.4_aarch64_a53_all_sdk_22.03.7.run"); //点击网站打开新窗口
 }
